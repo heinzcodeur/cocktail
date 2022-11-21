4ff6025fdabe7f9cf0aca1edf731d8a1bb257634
heinzcodeur <heinzcodeur@gmail.com>
2022-11-21 01:17:52 +0100

    nouvelle branch

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1 style="color:red;font-family: Tahoma;border: 2px solid darkred;width: 65%">ERREUR 404</h1>
+    <h1 style="color:orangered;font-family: Tahoma;border: 2px solid darkred;width: 65%">ERREUR 404</h1>
 </template>
 
 <script>
